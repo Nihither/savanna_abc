846abd01be16318b9b49488a442199709b4c55a7
Andrew Semenov <semenov.an.sh@gmail.com>
2023-12-07 21:15:09 +0300

    change chat model

--- messenger/admin.py
+++ messenger/admin.py
@@ -5,7 +5,7 @@ from .models import Chat
 
 # Register your models here.
 class ChatAdmin(admin.ModelAdmin):
-    list_display = ['uuid', 'first_person', 'second_person']
+    list_display = ['uuid', 'manager', 'teacher', 'student']
     ordering = ['uuid']
 
 
--- messenger/models.py
+++ messenger/models.py
@@ -5,13 +5,15 @@ import uuid
 # Create your models here.
 class Chat(models.Model):
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False, verbose_name="UUID")
-    first_person = models.ForeignKey(to='stuff.Profile', on_delete=models.CASCADE, related_name='chats_first_person',
-                                     editable=False, verbose_name="Профиль 1")
-    second_person = models.ForeignKey(to='stuff.Profile', on_delete=models.CASCADE, related_name='chats_second_person',
-                                      editable=False, verbose_name="Профиль 2")
+    manager = models.ForeignKey(to='stuff.Profile', on_delete=models.CASCADE, related_name='manager_chats',
+                                blank=True, null=True, verbose_name="Менеджер")
+    teacher = models.ForeignKey(to='stuff.Profile', on_delete=models.CASCADE, related_name='teacher_chats',
+                                blank=True, null=True, verbose_name="Преподаватель")
+    student = models.ForeignKey(to='stuff.Profile', on_delete=models.CASCADE, related_name='student_chat',
+                                blank=True, null=True, verbose_name="Студент")
 
     class Meta:
-        unique_together = ('first_person', 'second_person')
+        unique_together = ('manager', 'teacher', 'student')
 
     def __str__(self):
-        return f"{self.first_person} - {self.second_person}"
+        return f"{self.uuid}"
